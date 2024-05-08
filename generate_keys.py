72791369270107051fd0b91fc0b588e9be8707fd
AngelicaGrecilia <angelica.sidabutar@mail.ugm.ac.id>
2024-05-08 23:33:09 +0700

    first commit

--- generate_keys.py
+++ generate_keys.py
@@ -1,13 +1,19 @@
 import pickle
 from pathlib import Path
 import streamlit_authenticator as stauth
+from streamlit_authenticator.utilities.hasher import Hasher
 
-name = ["grecilia"]
-username = ["greciliahappy"]
-password = ["passwrod"]
+names = ["grecilia"]
+usernames = ["happy"]
+passwords = ["XXX"]
 
-hashed_password = stauth.Hasher(password).generate()
+hashed_password = Hasher(passwords).generate()
+
+# Menghasilkan hash untuk kata sandi
+#hashed_password = bcrypt.hashpw(passwords.encode(), bcrypt.gensalt())
+#hashed_password_str = hashed_password.decode()
 
 file_path = Path(__file__).parent / "hashed_pw.pkl"
 with file_path.open("wb") as file:
     pickle.dump(hashed_password, file)
+
